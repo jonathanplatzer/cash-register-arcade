76d6758cb38eeaf7449ca83870a71a3938319f90
Andreas Reicher <and.rei@gmx.at>
2014-10-21 15:40:54 +0200

    OptionsMenu

--- app/js/State/OptionMenu.js
+++ app/js/State/OptionMenu.js
@@ -10,10 +10,55 @@ var State = namespace('CashRegisterArcade.State');
  * @classdesc State for the option menu of the game
  * @memberOf CashRegisterArcade.State
  */
-State.OptionMenu = function(game) {};
+State.OptionMenu = function(game) {
+};
 
 State.OptionMenu.prototype = {
-    preload: function() {},
-    create: function() {},
-    update: function() {}
+    preload: function() {
+        this.game.load.image('button_theme','/assets/img/theme.png');
+        this.game.load.image('button_music','/assets/img/music.png');
+        this.game.load.image('button_sound','/assets/img/sound.png');
+    },
+    create: function() {
+        this.button_theme=this.game.add.button(this.game.width/10,this.game.height/10,'button_theme',this.toggleTheme);
+        this.button_theme.width=this.game.width/10*8;
+        this.button_theme.height=this.game.height/5;
+        this.button_music=this.game.add.button(this.game.width/10,this.game.height/10*4,'button_music',this.toggleMusic);
+        this.button_music.width=this.game.width/10*8;
+        this.button_music.height=this.game.height/5;
+        this.button_sound=this.game.add.button(this.game.width/10,this.game.height/10*7,'button_sound',this.toggleSfx);
+        this.button_sound.width=this.game.width/10*8;
+        this.button_sound.height=this.game.height/5;
+    },
+    update: function() {},
+    toggleMusic: function()
+    {
+        if(this.game.music===true){
+            this.game.music=false;
+        }
+        else{
+            this.game.music=true;
+        }
+        console.log(this.game.music);
+    },
+    toggleSfx: function()
+    {
+        if(this.game.sfx===true){
+            this.game.sfx=false;
+        }
+        else{
+            this.game.sfx=true;
+        }
+        console.log(this.game.sfx);
+    },
+    toggleTheme: function()
+    {
+        if(isNaN(this.game.theme)){
+            this.game.theme=0;
+        }
+        else{
+            this.game.theme++;
+        }
+        console.log(this.game.theme);
+    }
 };
