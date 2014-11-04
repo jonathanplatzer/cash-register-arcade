97b12dd8a2f11ec31f5f928d0970f5af7b2ab232
Andreas Reicher <and.rei@gmx.at>
2014-11-04 15:35:36 +0100

    Added Back Button

--- app/js/State/OptionMenu.js
+++ app/js/State/OptionMenu.js
@@ -1,6 +1,6 @@
 /**
  * @file See {@link CashRegisterArcade.State.OptionMenu}
- * @author Jonathan Platzer <plajoa10@htlkaindorf.at>
+ * @author Andreas Reicher <reiana10@htlkaindorf.at>
  */
 var State = namespace('CashRegisterArcade.State');
 
@@ -18,17 +18,22 @@ State.OptionMenu.prototype = {
         this.game.load.image('button_theme','/assets/img/theme.png');
         this.game.load.image('button_music','/assets/img/music.png');
         this.game.load.image('button_sound','/assets/img/sound.png');
+        this.game.load.image('button_back','/assets/img/back.png');
     },
     create: function() {
-        this.button_theme=this.game.add.button(this.game.width/10,this.game.height/10,'button_theme',this.toggleTheme);
-        this.button_theme.width=this.game.width/10*8;
-        this.button_theme.height=this.game.height/5;
-        this.button_music=this.game.add.button(this.game.width/10,this.game.height/10*4,'button_music',this.toggleMusic);
+        buttons=4;
+        this.button_theme=this.game.add.button(this.game.width/10,this.game.height/(2*(buttons+2)),'button_theme',this.toggleTheme);
+        this.button_theme.width=this.game.width/(10)*8;
+        this.button_theme.height=this.game.height/(buttons+2);
+        this.button_music=this.game.add.button(this.game.width/10,this.game.height/(2*(buttons+2))*4,'button_music',this.toggleMusic);
         this.button_music.width=this.game.width/10*8;
-        this.button_music.height=this.game.height/5;
-        this.button_sound=this.game.add.button(this.game.width/10,this.game.height/10*7,'button_sound',this.toggleSfx);
+        this.button_music.height=this.game.height/(buttons+2);
+        this.button_sound=this.game.add.button(this.game.width/10,this.game.height/(2*(buttons+2))*7,'button_sound',this.toggleSfx);
         this.button_sound.width=this.game.width/10*8;
-        this.button_sound.height=this.game.height/5;
+        this.button_sound.height=this.game.height/(buttons+2);
+        this.button_back=this.game.add.button(this.game.width/10,this.game.height/(2*(buttons+2))*10,'button_back',this.toMainMenu);
+        this.button_back.width=this.game.width/10*8;
+        this.button_back.height=this.game.height/(buttons+2);
     },
     update: function() {},
     toggleMusic: function()
@@ -60,5 +65,10 @@ State.OptionMenu.prototype = {
             this.game.theme++;
         }
         console.log(this.game.theme);
+    },
+    toMainMenu: function()
+    {
+        
+        this.game.state.start('mainMenu');
     }
 };
