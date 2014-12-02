fdbcb8305d334efbb1a93fa5f8e4e2b5a3488d76
Andreas Reicher <and.rei@gmx.at>
2014-12-02 15:21:05 +0100

    Physics fix. FUCK YEAH

--- app/js/State/Play.js
+++ app/js/State/Play.js
@@ -11,9 +11,65 @@ var State = namespace('CashRegisterArcade.State');
  * @memberOf CashRegisterArcade.State
  */
 State.Play = function(game) {};
-
 State.Play.prototype = {
-    preload: function() {},
-    create: function() {},
-    update: function() {}
+    preload: function() {
+        this.game.load.image('player', '/assets/img/player.png');
+    },
+    create: function() {
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.stage.backgroundColor = '#000000';
+        this.game.physics.arcade.gravity.y = 500;
+        player = this.game.add.sprite(1000, 500, 'player');
+        player.scale.setTo(0.1875);
+        this.game.physics.enable(player, Phaser.Physics.ARCADE);
+        player.body.bounce.y = 0.5;
+        player.body.collideWorldBounds = true;
+        //player.body.setSize(20, 32, 5, 16);
+        //this.game.camera.follow(player);
+        cursors = this.game.input.keyboard.createCursorKeys();
+
+        player.accelerationX=0;
+        player.accelerationY=0;
+        acceleration=20;
+        deceleration=10;
+        worldVelocity=-15;
+        jumpSpeed=-600;
+        player.maxVelocity=75;
+    },
+    update: function() {
+        player.accelerationX=0;
+        if (cursors.left.isDown)
+        {
+            player.accelerationX = -acceleration;
+            if(player.body.onFloor())
+            {
+                player.accelerationX +=worldVelocity;
+            }
+            else{
+                player.accelerationX/=deceleration;
+            }
+        }
+        else if (cursors.right.isDown)
+        {
+            player.accelerationX = acceleration;
+            if(player.body.onFloor())
+            {
+                player.accelerationX +=worldVelocity;
+            }
+            else{
+                player.accelerationX/=deceleration;
+            }
+        }
+        else if(player.body.onFloor())
+        {
+            player.accelerationX=player.body.velocity.x/-deceleration+worldVelocity;
+            
+        }
+
+        if (cursors.up.isDown && player.body.onFloor())
+        {
+            player.body.velocity.y = jumpSpeed;
+        }
+        player.body.velocity.x=player.body.velocity.x+player.accelerationX
+    }
 };
